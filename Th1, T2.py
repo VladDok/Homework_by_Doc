6a054ac128996fb7562359c894528c56e6731744
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-26 11:33:46 +0300

    Update Th1, T2.py

--- Th1, T2.py
+++ Th1, T2.py
@@ -1,3 +1,14 @@
+# Create a python program named “task2”, and use the built-in function `print` in it several times. Try to pass “sep”, “end” params and pass several parameters separated by commas. 
+
+# Also, provide a comment text above each print statement, mentioned above, with the expected output after execution of the particular print statement.
+
+# (Ex.
+# ‘hello world’
+# print(“hello world”)
+# )
+
+# My solve
+
 #Hello world
 
 print('Hello', 'world', sep=' ')
