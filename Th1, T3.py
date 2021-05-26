4d32f7fafa019bed19e93fa8c44e84cee350c5da
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-26 11:35:28 +0300

    Update Th1, T3.py

--- Th1, T3.py
+++ Th1, T3.py
@@ -1,3 +1,20 @@
+# Write a program, which has two print statements to print the following text (capital letters “O” and “H”, made from “#” symbols):
+
+#########
+#       #
+#       #
+#       #
+#########
+
+#       #
+#       #
+######### 
+#       # 
+#       #
+# Pay attention that usage of spaces is forbidden, as well as creating the whole result text string using “”” ”””, use ‘\n’ and ‘\t’ symbols instead.
+
+# My solution
+
 #Make text
 print('#'*9, end='\n')
 print('#', '#', sep='\t\t', end='\n')
