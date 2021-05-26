23cbe865202ff6e9a4773a2041311d6fe73e2346
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-26 11:43:34 +0300

    Update Th4, T2.py

--- Th4, T2.py
+++ Th4, T2.py
@@ -1,3 +1,11 @@
+# The birthday greeting program.
+
+# Write a program that takes your name as input, and then your age as input and greets you with the following:
+
+# “Hello <name>, on your next birthday you’ll be <age+1> years”   
+
+# My solution
+
 while True:
     answer_1 = input('What is your name?\n').capitalize()
     i = 0
