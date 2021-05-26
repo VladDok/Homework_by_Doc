d5ce1614e6a30239dc1174f55ba3793e0818609a
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-26 11:37:18 +0300

    Update Th2, T1.py

--- Th2, T1.py
+++ Th2, T1.py
@@ -1,6 +1,13 @@
-# Manipulate strings.
+# The greeting program.
 
-# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+# Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+
+#      “Good day <name>! <day> is a perfect day to learn some python.”
+ 
+
+# Note that <name> and <day> are predefined variables in source code.
+
+# An additional bonus will be to use different string formatting methods for constructing result string.
 
 # My solution
 
