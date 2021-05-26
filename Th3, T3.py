0df7da744b53e5564cc1b1c62e89f68625750e69
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-26 11:42:11 +0300

    Update Th3, T3.py

--- Th3, T3.py
+++ Th3, T3.py
@@ -1,3 +1,11 @@
+# The name check.
+
+# Write a program that has a variable with your name stored (in lowercase) and then asks for your name as input. 
+# The program should check if your input is equal to the stored name even if the given name has another case, e.g., 
+# if your input is “Anton” and the stored name is “anton”, it should return True.
+
+# My solution
+
 base_name = []
 
 print('Доброго дня! Уведіть Ваше ім\'я для збереження у нашій базі даних та подальшого звірення з нею.')
