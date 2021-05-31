6e0dcbc17bd0feb7b46b786dc33d81bcfd3c6d3f
VladDok <84400404+VladDok@users.noreply.github.com>
2021-05-31 23:43:12 +0300

    Add files via upload

---
+++ Тема 7/Th7, T1.py
@@ -0,0 +1,8 @@
+# A simple function.
+
+# Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie. The function should then print “My favorite movie is named {name}”.
+
+def favorite_movie(name):
+        return print('My favorite movie is named ' + name)
+
+favorite_movie('Super man')
---
+++ Тема 7/Th7, T3.py
@@ -0,0 +1,37 @@
+# A simple calculator.
+
+# Create a function called make_operation, which takes in a simple arithmetic 
+# operator as a first parameter (to keep things simple let it only be ‘+’, ‘-’ or ‘*’) 
+# and an arbitrary number of arguments (only numbers) as the second parameter. 
+# Then return the sum or product of all the numbers in the arbitrary parameter. For example:
+
+# the call make_operation(‘+’, 7, 7, 2) should return 16
+# the call make_operation(‘-’, 5, 5, -10, -20) should return 30
+# the call make_operation(‘*’, 7, 6) should return 42  
+
+def make_operation(operator, *args):
+    total = args[0]
+    i = 1
+    while i < len(args):
+        if type(args[i]) == str:
+            print('Don\'t use string!')
+            return None
+        else:
+            if operator == '+':
+                total += args[i]
+                i += 1
+            elif operator == '-':
+                total -= args[i]
+                i += 1
+            elif operator == '*':
+                total *= args[i]
+                i += 1
+            else:
+                print('Such operator does not exist. Please, try again!')
+                return None
+    return print(total)
+
+
+make_operation('+', 7, 7, 2)
+make_operation('-', 5, 5, -10, -20)
+make_operation('*', 7, 6)
