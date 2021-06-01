d9f309a49e97218ef08363dfcad94e8ee41699b0
VladDok <84400404+VladDok@users.noreply.github.com>
2021-06-01 22:50:24 +0300

    Update Th5, T2.py

--- Тема 5/Th5, T2.py
+++ Тема 5/Th5, T2.py
@@ -1,8 +1,8 @@
 # Exclusive common numbers.
 
-# Generate 2 lists with the length of 10 with random 
-# integers from 1 to 10, and make a third list containing 
-# the common integers between the 2 initial lists 
+# Generate 2 lists with the length of 10 with random
+# integers from 1 to 10, and make a third list containing
+# the common integers between the 2 initial lists
 # without any duplicates.
 
 # Constraints: use only while loop and random module to generate numbers
@@ -19,14 +19,14 @@ while i < 10:
     random_number_for_first_list = randint(1, 10)
     first_random_list.append(random_number_for_first_list)
     i += 1
-    
+
 i = 0
 while i < 10:
     random_number_for_second_list = randint(1, 10)
     second_random_list.append(random_number_for_second_list)
     i += 1
 
-common_random_list = first_random_list + second_random_list
-original_random_list = list(set(common_random_list))
+original_random_list = set(first_random_list) & set(second_random_list)
 
 print('The result is:', original_random_list)
+
