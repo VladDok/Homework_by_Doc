0b336656dce0c8be85e16e9b09dce3702287123f
VladDok <84400404+VladDok@users.noreply.github.com>
2021-06-28 17:59:53 +0300

    Update Th14, T1.py

--- Тема 14/Th14, T1.py
+++ Тема 14/Th14, T1.py
@@ -1,33 +1,22 @@
 from functools import wraps
-
+from inspect import getcallargs
 
 def logger(func):
     @wraps(func)
     def wrap(*args):
-        print(func.__name__ + ' called with ' + str([func(*args).__closure__[i].cell_contents for i in range(len(func(*args).__closure__))]) + '.')
+        print(func.__name__ + ' called with ' + str(getcallargs(func, *args)) + '.')
     return wrap
-
-        
+      
 @logger
 def add(x, y):
-    def add_1():
-        return x + y
-    return add_1
+    return x + y
 
 @logger
 def square_all(*args):
-    def square_all_1():
-        return [arg ** 2 for arg in args]
-    return square_all_1
+    return [arg ** 2 for arg in args]
 
 
 if __name__ == '__main__':
     add(4, 5)
     print('')
     square_all(1, 2, 3)   
-
-
-
-        
-
-
