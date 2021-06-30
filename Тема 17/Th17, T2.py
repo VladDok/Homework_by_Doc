0bad5034c1a567e09c24875c6d86494f279177f2
VladDok <84400404+VladDok@users.noreply.github.com>
2021-07-01 01:47:39 +0300

    Update Th17, T2.py

--- Тема 17/Th17, T2.py
+++ Тема 17/Th17, T2.py
@@ -1,3 +1,5 @@
+# Write tests for the Phonebook application, which you have implemented in module 1. Design tests for this solution and write tests using unittest library
+
 from faker import Faker
 import JSON
 import os
