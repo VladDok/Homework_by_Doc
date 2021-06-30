0e2de280a3e7900654de5c2ddd4f7e1bef387838
VladDok <84400404+VladDok@users.noreply.github.com>
2021-07-01 01:47:05 +0300

    Update Th17, T1.py

--- Тема 17/Th17, T1.py
+++ Тема 17/Th17, T1.py
@@ -1,3 +1,7 @@
+# Imports practice
+
+# Make a directory with 2 modules; make a function in one of them; then import this function in the other module and use that in your script of choice.
+
 from faker import Faker
 import unittest
 from test_class import Person
